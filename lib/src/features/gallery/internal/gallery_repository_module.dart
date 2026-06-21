28d82060f13d27663f788dd2a924e65f21016c5a
krllmp4 <pogrebnyakkir@gmail.com>
2026-06-21 21:16:46 +0300

    update

--- lib/src/features/gallery/internal/gallery_repository_module.dart
+++ lib/src/features/gallery/internal/gallery_repository_module.dart
@@ -1,10 +1,13 @@
-import 'package:liza_photo_web/src/features/gallery/data/repository/mock/mock_gallery_repository.dart';
+import 'package:liza_photo_web/src/features/gallery/data/repository/gallery_repository.dart';
 import 'package:liza_photo_web/src/features/gallery/domain/repository/i_gallery_repository.dart';
+import 'package:liza_photo_web/src/features/gallery/internal/api/gallery_service_module.dart';
 
 abstract final class GalleryRepositoryModule {
   static IGalleryRepository? _repository;
 
   static IGalleryRepository getRepository() {
-    return _repository ??= const MockGalleryRepository();
+    return _repository ??= GalleryRepository(
+      galleryService: GalleryServiceModule.getService(),
+    );
   }
 }
